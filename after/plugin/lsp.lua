59d560f0edc6187fe58fec08402adc41412eb47b
Óscar Belmonte Fernández <oscar.belmonte@uji.es>
2023-03-14 10:29:25 +0100

    Basado en ThePrimeagen.

---
+++ after/plugin/lsp.lua
@@ -0,0 +1,18 @@
+local lsp = require('lsp-zero').preset({
+  name = 'minimal',
+  set_lsp_keymaps = true,
+  manage_nvim_cmp = true,
+  suggest_lsp_servers = false,
+})
+
+-- (Optional) Configure lua language server for neovim
+lsp.nvim_workspace()
+
+lsp.on_attach(function(client, bufnr)
+	print("help")
+  local opts = {buffer = bufnr, remap = false}
+
+  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+end)
+
+lsp.setup()
