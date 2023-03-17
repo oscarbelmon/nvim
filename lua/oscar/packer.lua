6879aae8c5fc0ea9c9172f74b8cea418bb88a07e
Óscar Belmonte Fernández <oscar.belmonte@uji.es>
2023-03-17 17:50:05 +0100

    Añadido el plugin gitsigns.

--- lua/oscar/packer.lua
+++ lua/oscar/packer.lua
@@ -7,42 +7,57 @@ return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
 
+  -- Búsqueda de ficheros y cadenas dentro de ficheros
   use {
 	  'nvim-telescope/telescope.nvim', tag = '0.1.1',
 	  -- or                            , branch = '0.1.x',
 	  requires = { {'nvim-lua/plenary.nvim'} }
   } 
 
+  -- Colores
   use({ 'rose-pine/neovim', as = 'rose-pine' })
 
   vim.cmd('colorscheme rose-pine')
 
+  -- Resaltado de sintaxis
   use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+  -- Navegación rápida por ficheros que se añaden a un buffer
   use('theprimeagen/harpoon')
+  -- Árbol de cambios en el fichero actual.
   use('mbbill/undotree')
+  -- Interacción con git desde nvim
   use('tpope/vim-fugitive')
+  -- Envia código a un REPL que puede ser de Julia o de Python
+  use('jpalardy/vim-slime')
 
+  -- Para cargar Language Server Protocol
   use {
-  'VonHeikemen/lsp-zero.nvim',
-  branch = 'v1.x',
-  requires = {
-    -- LSP Support
-    {'neovim/nvim-lspconfig'},             -- Required
-    {'williamboman/mason.nvim'},           -- Optional
-    {'williamboman/mason-lspconfig.nvim'}, -- Optional
+      'VonHeikemen/lsp-zero.nvim',
+      branch = 'v1.x',
+      requires = {
+          -- LSP Support
+          {'neovim/nvim-lspconfig'},             -- Required
+          {'williamboman/mason.nvim'},           -- Optional
+          {'williamboman/mason-lspconfig.nvim'}, -- Optional
 
-    -- Autocompletion
-    {'hrsh7th/nvim-cmp'},         -- Required
-    {'hrsh7th/cmp-nvim-lsp'},     -- Required
-    {'hrsh7th/cmp-buffer'},       -- Optional
-    {'hrsh7th/cmp-path'},         -- Optional
-    {'saadparwaiz1/cmp_luasnip'}, -- Optional
-    {'hrsh7th/cmp-nvim-lua'},     -- Optional
+          -- Autocompletion
+          {'hrsh7th/nvim-cmp'},         -- Required
+          {'hrsh7th/cmp-nvim-lsp'},     -- Required
+          {'hrsh7th/cmp-buffer'},       -- Optional
+          {'hrsh7th/cmp-path'},         -- Optional
+          {'saadparwaiz1/cmp_luasnip'}, -- Optional
+          {'hrsh7th/cmp-nvim-lua'},     -- Optional
 
-    -- Snippets
-    {'L3MON4D3/LuaSnip'},             -- Required
-    {'rafamadriz/friendly-snippets'}, -- Optional
+          -- Snippets
+          {'L3MON4D3/LuaSnip'},             -- Required
+          {'rafamadriz/friendly-snippets'}, -- Optional
+      }
   }
-}
+
+  -- Resaltado de los cambios para git
+  use {
+      'lewis6991/gitsigns.nvim',
+      -- tag = 'release' -- To use the latest release (do not use this if you run Neovim nightly or dev builds!)
+  }
 
 end)
