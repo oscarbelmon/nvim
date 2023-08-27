b4d3bb24ee1ff5ccb76827c41c2225cb367f45d5
Óscar Belmonte Fernández <oscar.belmonte@uji.es>
2023-08-27 18:23:08 +0200

    Añadido plugin Neotree para visualizar estructura árbol proyecto.

---
+++ after/plugin/neotree.lua
@@ -0,0 +1,3 @@
+vim.keymap.set("n", "<leader>nt", "<cmd>Neotree<cr>",
+  {silent = true, noremap = true}
+)
--- lua/oscar/packer.lua
+++ lua/oscar/packer.lua
@@ -97,5 +97,15 @@ return require('packer').startup(function(use)
     },
  })
 
+ -- Visor de ficheros como árbol
+ use {
+  "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    requires = { 
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+    }
+  }
 
 end)
