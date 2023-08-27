69d40629a0e23545f9453df83f88a2aa50580271
Óscar Belmonte Fernández <oscar.belmonte@uji.es>
2023-08-27 20:37:11 +0200

    He cambiado la combinación de teclas para telescope.

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,6 +1,6 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<C-f>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>fs', function()
+vim.keymap.set('n', '<leader>tt', builtin.find_files, {})
+vim.keymap.set('n', '<C-t>', builtin.git_files, {})
+vim.keymap.set('n', '<leader>ts', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
