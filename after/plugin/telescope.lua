6b275032879a00b9006a0e04100e0ce9c00a1c56
Óscar Belmonte Fernández <oscar.belmonte@uji.es>
2023-10-02 08:44:29 +0200

    Cambiado un remap te --> Telescope grep search.

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,6 +1,6 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>tt', builtin.find_files, {})
 vim.keymap.set('n', '<C-t>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ts', function()
+vim.keymap.set('n', '<leader>te', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
