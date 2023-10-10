d7295853da0a07b73a1846f28f3fb1b5e94ae226
Óscar Belmonte Fernández <oscar.belmonte@uji.es>
2023-10-10 17:39:09 +0200

    Toggle de Neotree.

--- after/plugin/neotree.lua
+++ after/plugin/neotree.lua
@@ -1,4 +1,11 @@
-vim.keymap.set("n", "<leader>nt", "<cmd>Neotree<cr>",
+--vim.keymap.set("n", "<leader>nt", "<cmd>Neotree<cr>",
+--  {source = filesystem, 
+--  silent = true, 
+--  noremap = true
+--  }
+--)
+
+vim.keymap.set('n', '<leader>nt', '<cmd>Neotree toggle<cr>',
   {source = filesystem, 
   silent = true, 
   noremap = true
